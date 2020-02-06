ac4868045adbe8eda393e29c129594e324226fb7
vazark <vasanth93067@gmail.com>
2020-02-06 03:02:05 +0100

    Identified how to update canvas: draw face and restructure

--- gtest.py
+++ gtest.py
@@ -3,11 +3,11 @@ import time
 from dlib_68_ref import refShape
 
 # width of the animation window
-animation_window_width = 800
+animation_window_width = 600
 # height of the animation window
 animation_window_height = 600
 # delay between successive frames in seconds
-animation_refresh_seconds = 0.01
+animation_refresh_seconds = 0.2
 
 # The main window of the animation
 
@@ -24,13 +24,12 @@ def create_animation_window():
 
 def create_animation_canvas(window):
     canvas = tkinter.Canvas(window)
-    canvas.configure(bg="black")
+    canvas.configure(bg="blue")
     canvas.pack(fill="both", expand=True)
     return canvas
 
 # Draw the face and save to file
-
-
+# or actively stream the data to this screen
 def draw_face(window, canvas, shape):
     # Split Contours of the face
     j_shape = shape[0:17]
@@ -42,15 +41,30 @@ def draw_face(window, canvas, shape):
     m_shape = shape[48:68]
 
     # Draw Right eye
-    canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
-
-    canvas.move(ball, xinc, yinc)
+    tleft_x, _ = shape[37]
+    _ , tleft_y = shape[38]
+    
+    bright_x, _ = shape[40]
+    _ , bright_y = shape[41]
+    c = canvas.create_oval(tleft_x, tleft_y, bright_x, bright_y)
     window.update()
     time.sleep(animation_refresh_seconds)
-    ball_pos = canvas.coords(ball)
+    canvas.delete("all")
+    c = canvas.create_oval(tleft_x+30, tleft_y, bright_x, bright_y)
+    window.update()
+    time.sleep(animation_refresh_seconds)
+
+    # window.mainloop()
 
 
 # The actual execution starts here
 animation_window = create_animation_window()
 animation_canvas = create_animation_canvas(animation_window)
 draw_face(animation_window, animation_canvas, refShape)
+
+# # PIL create an empty image and draw object to draw on memory only, not visible
+# image1 = Image.new("RGB", (animation_window_width, animation_window_height), "white")
+# draw = ImageDraw.Draw(image1)
+# dra.create_oval(tleft_x, tleft_y, bright_x, bright_y)
+# filename = "test.jpg"
+# image1.save(filename)
